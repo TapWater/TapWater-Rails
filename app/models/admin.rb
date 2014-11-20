04758fa68997a40a63e05e89e7410b5276ba6d19
Cody Rogers <croge32@lsu.edu>
2014-11-20 16:29:38 -0600

    tried some new things, now getting 'invalid hash' error in sessions_controller

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 class Admin < ActiveRecord::Base
 	has_secure_password
 
-	attr_accessor :hash
+	attr_accessor :password_digest
 	include BCrypt
 
 	validates :password, length: { minimum: 6 }, confirmation: true
@@ -11,13 +11,13 @@ class Admin < ActiveRecord::Base
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }, presence: true, uniqueness: true
 
 	def admin_hash
-		salt = BCrypt::Engine.generate_salt
-     	self.hash = BCrypt::Engine.hash_secret(self.id, salt)
+		salt = self[:password_digest]
+     	hash = BCrypt::Engine.hash_secret(self.id, salt[0, 29].to_s)
     end
 
     def self.find_by_admin_hash(hash)
     	Admin.all.each do |admin|
-    		if admin.hash == hash
+    		if admin.admin_hash == hash
     			return admin
     		end
     	end
